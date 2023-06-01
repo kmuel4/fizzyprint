940008cde0ab75153a08696ba1bcf0f9fa75c964
Kurt Mueller <kmuel4@brockport.edu>
2023-06-01 17:49:48 -0400

    fixed offcanvas issue

--- src/pages/CheckoutPreview.js
+++ src/pages/CheckoutPreview.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Modal, Button, Badge, Container, Row } from "react-bootstrap";
+import {
+  Offcanvas,
+  Button,
+  Badge,
+  Container,
+  Row,
+  Card,
+} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../main.css";
 import {
@@ -10,10 +17,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CheckoutPreviewItem from "../components/Checkout/CheckoutPreviewItem";
 
 const CheckoutPreview = (props) => {
-  // handle Modal
+  // handle offcanvas
   const [show, setShow] = useState(true);
 
-  // close Modal
+  // close offcanvas
   const handleClose = () => {
     setShow(false);
     props.checkoutIndex();
@@ -29,80 +36,87 @@ const CheckoutPreview = (props) => {
   };
 
   return (
-      <Modal show={show} onHide={handleClose} fullscreen={true} dialogClassName="checkout-preview-modal">
-        <Modal.Header closeButton>
-          <Modal.Title style={{ fontWeight: "bold" }}>
-            {/* cart icon */}
-            <FontAwesomeIcon icon={faBagShopping} />
-            {/* cart badge */}
-            {props.cartItemsLength > 0 ? (
-              <Badge
-                pill
-                style={{
-                  position: "relative",
-                  top: "-1rem",
-                  left: "0rem",
-                  fontSize: ".75rem",
-                }}
-                className="bg-danger"
-              >
-                {props.cartItemsLength}
-              </Badge>
-            ) : (
-              <></>
-            )}
-            &nbsp; Shopping Cart
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {/* conditional cart message */}
-          {props.cartItemsLength === 0 ? (
-            <Container
-              className="d-flex flex-column align-items-center justify-content-center"
-              style={{ marginTop: "5rem" }}
+    <Offcanvas show={show} onHide={handleClose} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title style={{ fontWeight: "bold" }}>
+          {/* cart icon */}
+          <FontAwesomeIcon icon={faBagShopping} />
+          {/* cart badge */}
+          {props.cartItemsLength > 0 ? (
+            <Badge
+              pill
+              style={{
+                position: "relative",
+                top: "-1rem",
+                left: "0rem",
+                fontSize: ".75rem",
+              }}
+              className="bg-danger"
             >
-              <h1>
-                <FontAwesomeIcon
-                  icon={faTriangleExclamation}
-                  style={{ color: "#ffc107" }}
-                  size="xl"
-                />
-              </h1>
-              <p style={{ fontSize: "1rem" }}>
-                Your cart is empty, let's fix that...
-              </p>
-            </Container>
+              {props.cartItemsLength}
+            </Badge>
           ) : (
-            <Container>
-              <Row>
-                {props.cart.map((item) => {
-                  const card = props.cards.find((card) => card.id === item);
-                  return (
-                    <Container key={item}>
-                      <CheckoutPreviewItem
-                        key={item}
-                        card={card}
-                        item={item}
-                        remove={handleRemove}
-                      />
-                      <br />
-                    </Container>
-                  );
-                })}
-              </Row>
-            </Container>
+            <></>
           )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            className="w-100"
-            onClick={handleCheckout}
-            disabled={props.cartItemsLength === 0}
+          &nbsp; Shopping Cart
+        </Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        {/* conditional cart message */}
+        {props.cartItemsLength === 0 ? (
+          <Container
+            className="d-flex flex-column align-items-center justify-content-center"
+            style={{ marginTop: "5rem" }}
           >
-            Checkout
-          </Button>
-        </Modal.Footer>
-      </Modal>
+            <h1>
+              <FontAwesomeIcon
+                icon={faTriangleExclamation}
+                style={{ color: "#ffc107" }}
+                size="xl"
+              />
+            </h1>
+            <p style={{ fontSize: "1rem" }}>
+              Your cart is empty, let's fix that...
+            </p>
+          </Container>
+        ) : (
+          <Container style={{marginBottom: "3rem"}}>
+            <Row>
+              {props.cart.map((item) => {
+                const card = props.cards.find((card) => card.id === item);
+                return (
+                  <Container key={item}>
+                    <CheckoutPreviewItem
+                      key={item}
+                      card={card}
+                      item={item}
+                      remove={handleRemove}
+                    />
+                    <br />
+                  </Container>
+                );
+              })}
+            </Row>
+          </Container>
+        )}
+        <Container style={{
+              position: "absolute",
+              bottom: "0rem",
+              right: "0rem",
+              backgroundColor: "white",
+              padding: "1rem"
+            }}>
+
+            <Button
+              className="w-100"
+              onClick={handleCheckout}
+              disabled={props.cartItemsLength === 0}
+            >
+              Checkout
+            </Button>
+        </Container>
+      </Offcanvas.Body>
+    </Offcanvas>
   );
 };
 
